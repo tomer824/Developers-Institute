00c52b58efe5f5542ad655030efdba842c4984fa
Tomer <goldstein.ty@gmail.com>
2020-07-05 16:43:03 +0300

    update

--- Assignments and Daily Challeneges/Week 2/Day 5/XP/main.js
+++ Assignments and Daily Challeneges/Week 2/Day 5/XP/main.js
@@ -5,7 +5,7 @@ let operator = "";
 let total = 0;
 let equal;
 
-function my_f(button)
+function my_f(button){
 	if(button == "+" || button == "-" || button == "*" || button == "/"){
 		operator = button;
 		first_num = parseInt(num.join());
@@ -19,58 +19,24 @@ function my_f(button)
 	else {
 		num.push(button);
 	}
-
-function holding(array){
-	num 
-	// first num = convert array num to string and then to number
-	// num = []
-
 }
 
 
 function calc(num1, operator, num2){
-	total = // first_num operand num1 also turned into string and number
-	console.log(total)
+	switch(operator){
+		case operator == "+":
+			console.log(first_num + second_num)
+			// return first_num + second_num
+		case operator == "-":
+			console.log(first_num - second_num)
+			// return first_num - second_num
+		case operator == "*":
+			console.log(first_num * second_num)
+			// return first_num * second_num
+		case operator == "/":
+			console.log(first_num / second_num)
+			// return first_num / second_num	
+	}
 }
-
-
-my_f()
-
-
-
-// function my_f(button){
-// 	if(typeof(button) == 'string' && button != "="){
-// 		operator = button;
-// 		first_num = parseInt(num1.join(""));
-// 		num1 = []
-// 		console.log(first_num)
-// 	}
-// 	else if(button == "=") {
-// 		second_num = parseInt(num1.join(""));
-// 		console.log(second.num)
-
-// 		switch(operator){
-// 			case "+":
-// 				console.log(first_num + second_num)
-// 				return first_num + second_num;
-				
-// 			case "-":
-// 				console.log(first_num - second_num)
-// 				return first_num - second_num;
-				
-// 			case "/":
-// 				console.log(first_num / second_num)
-// 				return first_num / second_num;
-
-// 			case "*":
-// 				console.log(first_num * second_num)
-// 				return first_num * second_num;
-// 		}
-// 	}
-// 	else {
-// 	num1.push(button)
-// 	}
-// }
-
 
 my_f()
